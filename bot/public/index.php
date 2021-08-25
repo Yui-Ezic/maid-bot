e9fc56c8e13da7fb74491396a3e7bac34151b8c9
Yui-Ezic <mishan221199@gmail.com>
2021-08-26 00:12:04 +0300

    Moved app creation to file

--- bot/public/index.php
+++ bot/public/index.php
@@ -2,15 +2,14 @@
 
 declare(strict_types=1);
 
-use Slim\Factory\AppFactory;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
-
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
