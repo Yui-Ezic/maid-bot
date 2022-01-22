933dfe5aa5ca98e7942df5f5b56447ba5531a2da
Mykhailo <mishan221199@gmail.com>
2022-01-22 21:58:55 +0200

    refactor profanity: remove unused 'use' statement

--- bot/src/Profanity/Notifier/Notifier.php
+++ bot/src/Profanity/Notifier/Notifier.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Profanity\Notifier;
 
-use App\Profanity\Detector\ProfanityCollection;
-
 interface Notifier
 {
     public function notify(Notification $notification);
