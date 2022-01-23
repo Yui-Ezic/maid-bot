4b565da45063a743dd244fc5e24837637bd98034
Mykhailo <mishan221199@gmail.com>
2022-01-23 19:19:12 +0200

    fix: undefined 'Botman' namespace

--- bot/config/routes.php
+++ bot/config/routes.php
@@ -7,6 +7,4 @@ use Slim\App;
 
 return static function (App $app): void {
     $app->get('/', Http\Action\HomeAction::class);
-
-    $app->get('/botman', Http\Action\Botman\HandleAction::class);
 };
