cef491e6c4cf9d156a98466835482b443b04b163
Mykhailo <mishan221199@gmail.com>
2022-01-23 16:36:01 +0200

    tests bot: add tests for message profanity notifier

--- bot/src/Bot/Test/Unity/Profanity/MessageNotifierTest.php
+++ bot/src/Bot/Test/Unity/Profanity/MessageNotifierTest.php
@@ -4,8 +4,8 @@ namespace App\Bot\Test\Unity\Profanity;
 
 use App\Bot\Profanity\Notifier\MessageMaker;
 use App\Bot\Profanity\Notifier\MessageNotifier;
-use App\Platform\Interactor\Message;
 use App\Platform\Interactor\MessageSender;
+use App\Platform\Test\Unit\Interactor\MessageBuilder;
 use App\Profanity\Test\Unit\NotificationBuilder;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -35,6 +35,29 @@ class MessageNotifierTest extends TestCase
         $this->makeMessageNotifier()->notify($notification);
     }
 
+    public function testMakeMessageWithChatId()
+    {
+        $this->addRecipients($recipients = $this->getRandomRecipients());
+        $messageMakerMocks = $this->getMessageMakerMocks($recipients);
+
+        // expects withChatId will be called for each recipient
+        $this->messageMakerMock
+            ->expects($this->exactly(count($this->recipientChatIds)))
+            ->method('withChatId')
+            ->withConsecutive(...$this->consecutiveEqualTo($recipients))
+            ->willReturnOnConsecutiveCalls(...$messageMakerMocks);
+
+        // expects 'make' be called once for all mock returned from 'withChatId'
+        foreach ($messageMakerMocks as $messageMakerMock) {
+            $messageMakerMock
+                ->expects($this->once())
+                ->method('make')
+                ->willReturn(MessageBuilder::random()->build());
+        }
+
+        $this->makeMessageNotifier()->notify(NotificationBuilder::random()->build());
+    }
+
     private function makeMessageNotifier(): MessageNotifier
     {
         return new MessageNotifier($this->recipientChatIds, $this->messageSenderMock, $this->messageMakerMock);
@@ -43,5 +66,37 @@ class MessageNotifierTest extends TestCase
     private function addRecipients(array $chatIds)
     {
         $this->recipientChatIds = array_merge($this->recipientChatIds, $chatIds);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getRandomRecipients(): array
+    {
+        $randomRange = range(1, rand(1,3));
+        return array_map(static function (int $value) {
+            return uniqid((string)$value);
+        }, $randomRange);
+    }
+
+    /**
+     * @param string[] $recipients
+     * @return MockObject[]
+     */
+    private function getMessageMakerMocks(array $recipients): array
+    {
+        $messageMakerMocks = [];
+        foreach ($recipients as $recipient) {
+            $mock = $this->createMock(MessageMaker::class);
+            $messageMakerMocks[$recipient] = $mock;
+        }
+        return $messageMakerMocks;
+    }
+
+    private function consecutiveEqualTo(array $values): array
+    {
+        return array_map(function (mixed $value) {
+            return [$this->equalTo($value)];
+        }, $values);
     }
 }
---
+++ bot/src/Platform/Test/Unit/Interactor/MessageBuilder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Platform\Test\Unit\Interactor;
+
+use App\Platform\Interactor\Message;
+
+class MessageBuilder
+{
+    private const CHAT_ID = 'chatId';
+    private const TEXT = 'text';
+
+    private function __construct(private array $properties)
+    {
+    }
+
+    public function build(): Message
+    {
+        return new Message($this->properties[self::CHAT_ID], $this->properties[self::TEXT]);
+    }
+
+    public static function random(): self
+    {
+        return new self([
+            self::CHAT_ID => uniqid(),
+            self::TEXT => uniqid()
+        ]);
+    }
+
+    private function with(string $property, mixed $value): self
+    {
+        $clone = clone $this;
+        $clone->properties[$property] = $value;
+        return $clone;
+    }
+}
