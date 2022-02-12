a0207bfcf0945b1f70a85299621c60f242f13d54
Yui-Ezic <mishan221199@gmail.com>
2022-02-12 17:29:21 +0200

    tests: create functional tests for vk confirmation

---
+++ bot/tests/Functional/WebTestCase.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+/**
+ * @internal
+ */
+abstract class WebTestCase extends TestCase
+{
+    private ?App $app = null;
+
+    protected function tearDown(): void
+    {
+        $this->app = null;
+        parent::tearDown();
+    }
+
+    protected static function json(string $method, string $path, array $body = []): ServerRequestInterface
+    {
+        $request = self::request($method, $path)
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('Content-Type', 'application/json');
+        $request->getBody()->write(json_encode($body, JSON_THROW_ON_ERROR));
+        return $request;
+    }
+
+    protected static function html(string $method, string $path, array $body = []): ServerRequestInterface
+    {
+        $request = self::request($method, $path)
+            ->withHeader('Accept', 'text/html')
+            ->withHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $request->getBody()->write(http_build_query($body));
+        return $request;
+    }
+
+    protected static function request(string $method, string $path): ServerRequestInterface
+    {
+        return (new ServerRequestFactory())->createServerRequest($method, $path);
+    }
+
+    protected function app(): App
+    {
+        if ($this->app === null) {
+            $this->app = (require __DIR__ . '/../../config/app.php')($this->container());
+        }
+        return $this->app;
+    }
+
+    private function container(): ContainerInterface
+    {
+        /** @var ContainerInterface */
+        return require __DIR__ . '/../../config/container.php';
+    }
+}
