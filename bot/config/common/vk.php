53b35bc7564ac80a8726da498afc3989e0a9e2e7
Yui-Ezic <mishan221199@gmail.com>
2022-02-13 06:52:20 +0200

    fix: wrong psalm annotations

--- bot/config/common/vk.php
+++ bot/config/common/vk.php
@@ -30,21 +30,19 @@ return [
     CallbackHandler::class => static function (ContainerInterface $container) {
         /**
          * @psalm-suppress MixedAssignment
-         * @psaml-var array{
-         *   vk:array{
-         *     handlers:array<string,CallbackHandler>
-         *   },
-         *   secret: string
+         * @var array{
+         *     vk:array{
+         *         handlers:array<string,string>,
+         *         secret:string
+         *     },
          * } $config
          */
         $config = $container->get('config');
 
-        /**
-         * @psalm-suppress MissingClosureReturnType
-         * @psalm-suppress MixedArrayAccess
-         * @psalm-suppress MixedArgument
-         */
-        $handlers = array_map(static fn (string $name) => $container->get($name), $config['vk']['handlers']);
+        $handlers = array_map(static function (string $name) use ($container): CallbackHandler {
+            /** @var CallbackHandler */
+            return $container->get($name);
+        }, $config['vk']['handlers']);
 
         /**
          * @psalm-suppress MixedArrayAccess
@@ -56,40 +54,33 @@ return [
     ConfirmationCallbackHandler::class => static function (ContainerInterface $container) {
         /**
          * @psalm-suppress MixedAssignment
-         * @psaml-var array{api:array{confirmationCode:string}, groupId:string} $config
+         * @var array{
+         *     vk:array{
+         *         api:array{confirmationCode:string},
+         *         groupId:int
+         *     }
+         * } $config
          */
         $config = $container->get('config');
 
-        /**
-         * @psalm-suppress MixedArrayAccess
-         * @psalm-suppress MixedArgument
-         */
         return new ConfirmationCallbackHandler($config['vk']['api']['confirmationCode'], $config['vk']['groupId']);
     },
     VKApiClient::class => static function (ContainerInterface $container) {
         /**
          * @psalm-suppress MixedAssignment
-         * @psaml-var array{api:array{version:string}} $config
+         * @var array{vk:array{api:array{version:string}}} $config
          */
         $config = $container->get('config');
 
-        /**
-         * @psalm-suppress MixedArrayAccess
-         * @psalm-suppress MixedArgument
-         */
         return new VKApiClient($config['vk']['api']['version']);
     },
     ApiMessageSender::class => static function (ContainerInterface $container) {
         /**
          * @psalm-suppress MixedAssignment
-         * @psaml-var array{api:array{accessToken:string}} $config
+         * @var array{vk:array{api:array{accessToken:string}}} $config
          */
         $config = $container->get('config');
 
-        /**
-         * @psalm-suppress MixedArrayAccess
-         * @psalm-suppress MixedArgument
-         */
         return new ApiMessageSender($container->get(VKApiClient::class), $config['vk']['api']['accessToken']);
     },
 ];
