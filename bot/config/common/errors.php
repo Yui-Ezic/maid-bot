868c4c6365918ccf2cee965d1079c7de846d1ca3
Yui-Ezic <mishan221199@gmail.com>
2022-02-20 01:08:35 +0200

    feat: log uncaught errors by psr logger

--- bot/config/common/errors.php
+++ bot/config/common/errors.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\ErrorHandler\LogErrorHandler;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Log\LoggerInterface;
 use Slim\Interfaces\CallableResolverInterface;
 use Slim\Middleware\ErrorMiddleware;
 use function App\env;
@@ -18,14 +20,23 @@ return [
          */
         $config = $container->get('config')['errors'];
 
-        return new ErrorMiddleware(
+        $middleware = new ErrorMiddleware(
             $callableResolver,
             $responseFactory,
             $config['display_details'],
             true,
             true
         );
+
+        $middleware->setDefaultErrorHandler($container->get(LogErrorHandler::class));
+
+        return $middleware;
     },
+    LogErrorHandler::class => static fn (ContainerInterface $container): LogErrorHandler => new LogErrorHandler(
+        $container->get(CallableResolverInterface::class),
+        $container->get(ResponseFactoryInterface::class),
+        $container->get(LoggerInterface::class)
+    ),
 
     'config' => [
         'errors' => [
