129f76e9c35f0bf942671b76702e9cc9adab8e4b
Yui-Ezic <mishan221199@gmail.com>
2022-02-20 00:42:57 +0200

    refactor: move error handler middleware creation to config

--- bot/config/middleware.php
+++ bot/config/middleware.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-return static function (Slim\App $app, Psr\Container\ContainerInterface $container): void {
-    /** @psalm-var array{debug:bool} */
-    $config = $container->get('config');
+use Slim\Middleware\ErrorMiddleware;
 
-    $app->addErrorMiddleware($config['debug'], true, true);
+return static function (Slim\App $app): void {
+    $app->add(ErrorMiddleware::class);
     $app->addBodyParsingMiddleware();
 };
